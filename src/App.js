7c28edd60034b9cb5b5c8c5ffadae26d4cc7dc33
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-12-03 14:50:29 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './components/navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='mt-5'>Hello world</h1>
+      <Navbar/>
     </div>
   );
 }
